3b350ea9b655202a25b90e8487f0171a2aaad169
TyrantLink <tyrantlink@pm.me>
2021-04-28 21:44:15 -0700

    fixed dumb.

--- justin's girlfriend/bot.py
+++ justin's girlfriend/bot.py
@@ -1,5 +1,6 @@
 import discord
+client = discord.Client()
 @client.event
 async def on_message(message):
-    if message.author.id == 434133025450754058: await message.channel.send('justib i lov you pls')
-discord.Client().run('ODM3MTgxNzQ1NzQwNjQ0MzYy.YIo0Qg.bN13rkJxHJykczzwiO2bIdIuZWk')
+    if message.author.id == 434133025450754058: await message.channel.send('justib i lov you plsb')
+client.run('ODM3MTgxNzQ1NzQwNjQ0MzYy.YIo0Qg.bN13rkJxHJykczzwiO2bIdIuZWk')
