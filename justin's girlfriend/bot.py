b62f24f7dadeac81040c46f0fbfa76073ecdb6a9
TyrantLink <tyrantlink@pm.me>
2021-04-28 21:34:47 -0700

    created justin's girlfriend

---
+++ justin's girlfriend/bot.py
@@ -0,0 +1,5 @@
+import discord
+@client.event
+async def on_message(message):
+    if message.author.id == 434133025450754058: await message.channel.send('justib i lov you pls')
+discord.Client().run('ODM3MTgxNzQ1NzQwNjQ0MzYy.YIo0Qg.bN13rkJxHJykczzwiO2bIdIuZWk')
